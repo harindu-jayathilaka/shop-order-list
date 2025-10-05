489e8849bc272506fd0858b5287b0b583da1f1c6
harindu-jayathilaka <139651166+harindu-jayathilaka@users.noreply.github.com>
2025-10-05 17:41:01 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,13 +9,9 @@ const filesToCache = [
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
-  );
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
